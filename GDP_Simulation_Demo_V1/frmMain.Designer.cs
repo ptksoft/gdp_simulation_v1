25fd293a99b7b7f27daab00e1d423f092c2c4283
Pitak @Surface <pitak1978@outlook.com>
2015-08-26 22:47:25 +0700

    Add New Files

---
+++ GDP_Simulation_Demo_V1/frmMain.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace GDP_Simulation_Demo_V1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnPrev5 = new System.Windows.Forms.Button();
+            this.btnNext5 = new System.Windows.Forms.Button();
+            this.tmPlaying = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBar
+            // 
+            this.tBar.LargeChange = 1;
+            this.tBar.Location = new System.Drawing.Point(27, 293);
+            this.tBar.Maximum = 120;
+            this.tBar.Name = "tBar";
+            this.tBar.Size = new System.Drawing.Size(1127, 45);
+            this.tBar.TabIndex = 0;
+            this.tBar.Scroll += new System.EventHandler(this.tBar_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(24, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "16.30";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(1115, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "18.30";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblTime.ForeColor = System.Drawing.Color.Blue;
+            this.lblTime.Location = new System.Drawing.Point(548, 259);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(87, 31);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "00.00";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnPlay.Location = new System.Drawing.Point(554, 344);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(65, 43);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnBegin.Location = new System.Drawing.Point(27, 346);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(67, 39);
+            this.btnBegin.TabIndex = 6;
+            this.btnBegin.Text = "|<";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnEnd.Location = new System.Drawing.Point(1087, 346);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(67, 39);
+            this.btnEnd.TabIndex = 7;
+            this.btnEnd.Text = ">|";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnPrev5
+            // 
+            this.btnPrev5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnPrev5.Location = new System.Drawing.Point(466, 344);
+            this.btnPrev5.Name = "btnPrev5";
+            this.btnPrev5.Size = new System.Drawing.Size(65, 43);
+            this.btnPrev5.TabIndex = 8;
+            this.btnPrev5.Text = "-5 <<";
+            this.btnPrev5.UseVisualStyleBackColor = true;
+            this.btnPrev5.Click += new System.EventHandler(this.btnPrev5_Click);
+            // 
+            // btnNext5
+            // 
+            this.btnNext5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnNext5.Location = new System.Drawing.Point(642, 344);
+            this.btnNext5.Name = "btnNext5";
+            this.btnNext5.Size = new System.Drawing.Size(65, 43);
+            this.btnNext5.TabIndex = 9;
+            this.btnNext5.Text = ">> +5";
+            this.btnNext5.UseVisualStyleBackColor = true;
+            this.btnNext5.Click += new System.EventHandler(this.btnNext5_Click);
+            // 
+            // tmPlaying
+            // 
+            this.tmPlaying.Interval = 1000;
+            this.tmPlaying.Tick += new System.EventHandler(this.tmPlaying_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 462);
+            this.Controls.Add(this.btnNext5);
+            this.Controls.Add(this.btnPrev5);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnBegin);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tBar);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GDP Simulation Demo";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.tBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnBegin;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnPrev5;
+        private System.Windows.Forms.Button btnNext5;
+        protected System.Windows.Forms.Timer tmPlaying;
+
+
+    }
+}
+
---
+++ GDP_Simulation_Demo_V1/frmMain.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GDP_Simulation_Demo_V1
+{
+    public partial class frmMain : Form
+    {
+        int sx = 50;    // Start X
+        int sy = 50;    // Start Y
+        int ph = 20;    // Panel Height
+        int pw = 5;     // Panel Width
+        Panel[] pn = new Panel[220];    // Panel
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            PrepareGDP();
+            this.Refresh();
+        }
+
+        private void PrepareGDP()
+        {
+            pw = (this.ClientSize.Width) / 210;
+
+            //MessageBox.Show("Width=" + w.ToString());
+                        
+            for (int i = 0; i < 220; i++)
+            {
+                pn[i] = new Panel();
+                pn[i].BorderStyle = BorderStyle.FixedSingle;
+                //pn[i].BackColor = Color.FromArgb(i, Math.Abs(220-i), 0);
+                pn[i].BackColor = Color.FromArgb(25+i, 255-i, 0);
+                pn[i].Size = new Size(pw, ph);
+                pn[i].Location = new Point(sx + (i * pw), sy);
+                this.Controls.Add(pn[i]);
+            }
+
+            // Scale Label
+            for (int i = 0; i < 22; i++)
+            {
+                Label lb = new Label();
+                lb.Text = i.ToString();
+                lb.Location = new Point(sx + ((i * 10) * pw), sy - 20);
+                lb.AutoSize = true;
+                this.Controls.Add(lb);
+            }
+
+            // Sensor            
+            ShowSensor("S1-M", 0);      // S1 - Microwave
+            ShowSensor("S2-V", 20);      // S2 - Video
+            ShowSensor("S3-V", 40);      // S3 - Video
+            ShowSensor("S4-V", 90);      // S4 - Video
+            ShowSensor("S5-V", 160);      // S5 - Video
+            ShowSensor("S6-V", 180);      // S6 - Video
+            ShowSensor("S7-V", 214);      // S7 - Microwave
+        }
+
+        private void ShowSensor(string sname, int i)
+        {
+            Label lb = new Label();
+            lb.Text = sname;
+            lb.Location = new Point(sx + (i * pw), sy + 25);
+            lb.AutoSize = true;
+            this.Controls.Add(lb);
+        }
+
+        private void tBar_Scroll(object sender, EventArgs e)
+        {
+            DateTime DT = new DateTime(2015, 1, 1, 16, 30, 0);
+            DT = DT.AddMinutes(tBar.Value);
+            lblTime.Text = DT.Hour.ToString() + "." + string.Format("{0:00}", DT.Minute);
+        }
+
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            StopPlaying();
+            tBar.Value = tBar.Minimum;
+            tBar_Scroll(this, new EventArgs());
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            StopPlaying();
+            tBar.Value = tBar.Maximum;
+            tBar_Scroll(this, new EventArgs());
+        }
+
+        private void btnPrev5_Click(object sender, EventArgs e)
+        {
+            StopPlaying();
+            int d = tBar.Value - 5;
+            if (tBar.Minimum <= d) tBar.Value = d;
+            else tBar.Value = tBar.Minimum;
+            tBar_Scroll(this, new EventArgs());
+        }
+
+        private void btnNext5_Click(object sender, EventArgs e)
+        {
+            StopPlaying();
+            int d = tBar.Value + 5;
+            if (tBar.Maximum >= d) tBar.Value = d;
+            else tBar.Value = tBar.Maximum;
+            tBar_Scroll(this, new EventArgs());
+        }
+
+        private void tmPlaying_Tick(object sender, EventArgs e)
+        {
+            if (tBar.Value >= tBar.Maximum)
+            {
+                tmPlaying.Enabled = false;
+            }
+            else
+            {
+                tBar.Value += 1;
+                tBar_Scroll(this, new EventArgs());
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (tmPlaying.Enabled)
+            {
+                tmPlaying.Stop();
+                btnPlay.Text = "Play";
+            }
+            else
+            {
+                tmPlaying.Start();
+                btnPlay.Text = "Stop";
+            }
+        }
+
+        private void StopPlaying()
+        {
+            tmPlaying.Stop();
+            btnPlay.Text = "Play";
+        }
+    }
+}
